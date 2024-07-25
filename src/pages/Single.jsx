ea87090fca85ec87cce05b985265c623974e0298
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-07-25 08:12:20 +0000

    update

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { FcStart} from "react-icons/fc";
 import styles from '../Styles/single.module.css'
 import NavBar from '../components/Navbar'
 import Footer from '../components/Footer'
 import CircleRating from '../components/CircleRating'
+import { MdHeight } from 'react-icons/md';
 const Single = () => {
   return (
     <>
@@ -14,7 +16,7 @@ const Single = () => {
             <div className={styles.title}> <h1> Movie Title</h1></div>
             <div className={styles.subTitle}><h3> Movie Sub Title</h3></div>
             <div className={styles.genre}><span>Genre</span></div>
-            <div className={styles.rating}><span><CircleRating/></span> <span>play button</span> <span>Watch Trailer</span></div>
+            <div className={styles.rating}><span><CircleRating/></span> <span><FcStart style={{fontSize:'82px'}} /></span><h2 style={{fontWeight:'500',fontSize:'20px'}}>Watch Trailer</h2></div>
             <div className={styles.heading}>Overview</div>
             <div className={styles.description}><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam pariatur id molestiae ratione suscipit quidem provident ea magnam soluta ipsam?</span></div>
             <div className={styles.info}><span>Status : </span> <span>Release Date : </span> <span>Runtime : </span></div><hr />
