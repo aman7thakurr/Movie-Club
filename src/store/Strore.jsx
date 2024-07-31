2db486591053aa4563cad53e69b59afa11bf4ebd
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-07-31 10:19:16 +0000

    a

--- src/store/Strore.jsx
+++ src/store/Strore.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import homeReducer from './slice/homeSlice';
 import moviesReducer from './slice/moviesSlice';
-import tvShowsReducer from './slice/tvSeriesSlice'
+// import tvShowsReducer from './slice/tvSeriesSlice'
 
 export const store = configureStore({
     reducer: {
