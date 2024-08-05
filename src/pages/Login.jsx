7682d9069c700159e0e44f061653f850f7142cc0
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-08-05 05:45:25 +0000

    import

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 // import Styles from '../Styles/login.module.css';
 import NavBar from '../components/Navbar';
 import Footer from '../components/Footer';
