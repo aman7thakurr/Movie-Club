daf074284b2f3504e4c88461a6da3819c41faab5
Aman Thakur <106446742+aman7thakurr@users.noreply.github.com>
2024-08-05 06:25:58 +0000

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,8 +18,6 @@ const Home = () => {
     
         document.querySelector('meta[property="og:url"]').setAttribute('content', url);
         document.querySelector('meta[property="og:title"]').setAttribute('content', title);
-        document.querySelector('meta[property="og:image"]').setAttribute('content', image);
-        document.querySelector('meta[property="og:description"]').setAttribute('content', description);
        
 
     
@@ -27,8 +25,6 @@ const Home = () => {
        
           document.querySelector('meta[property="og:url"]').removeAttribute('content');
           document.querySelector('meta[property="og:title"]').removeAttribute('content');
-          document.querySelector('meta[property="og:image"]').removeAttribute('content');
-          document.querySelector('meta[property="og:description"]').removeAttribute('content');
           
 
         };
